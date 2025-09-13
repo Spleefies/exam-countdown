4b9fedafd81188b465265fe4a1854e28590edede
Spleefies <cutecatty32@gmail.com>
2025-09-13 20:41:03 +0800

    1st commit 26d 17h 49m 33s

---
+++ countdown.js
@@ -0,0 +1,32 @@
+function countdown(date, id){
+    let examDate = new Date(date).getTime();
+    
+    let x = setInterval(() => {
+        let now = new Date().getTime();
+        let timeLeft = examDate - now;
+        
+        let days = Math.floor(timeLeft / 86400000);
+        let hours = Math.floor((timeLeft % 86400000) / 3600000);
+        let minutes = Math.floor((timeLeft % 3600000) / 60000);
+        let seconds = Math.floor((timeLeft % 60000) / 1000);
+        
+        document.getElementById(id).innerHTML =
+            days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        
+        if (timeLeft < 0) {
+            clearInterval(x);
+            document.getElementById(id).innerHTML = "its alr over lol";
+        }
+    }, 1000);
+};
+
+countdown("Oct 10, 2025 14:30:00", "EL1131");
+countdown("Oct 13, 2025 08:00:00", "CL1332");
+countdown("Oct 13, 2025 14:00:00", "MT");
+countdown("Oct 17, 2025 08:00:00", "BL1131");
+countdown("Oct 21, 2025 08:00:00", "CM1131");
+countdown("Oct 22, 2025 08:00:00", "PC1131");
+countdown("Oct 23, 2025 08:00:00", "MA1133");
+countdown("Oct 27, 2025 08:00:00", "3rdLang");
+
+
